de137c0791d85cb4222647433e502a93e42fe369
roaayala <roaayala.ahm@gmail.com>
2026-07-04 15:20:41 +0800

    Skill

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,22 @@
 import Contact from "./resumes/Contact";
 import PersonalInformation from "./resumes/PersonalInformation";
 import ProfessionalSummary from "./resumes/ProfessionalSummary";
+import Skill from "./resumes/Skill";
 
 export default function Resume({ data }) {
-  const { personalInformation, contact, professionalSummary } = data;
+  const {
+    personalInformation,
+    contact,
+    professionalSummary,
+    skill,
+    education,
+    employment,
+  } = data;
   return (
     <main className="w-full flex flex-col gap-8">
       <PersonalInformation data={personalInformation} />
       <ProfessionalSummary data={professionalSummary} />
+      <Skill data={skill} />
       <Contact data={contact} />
     </main>
   );
