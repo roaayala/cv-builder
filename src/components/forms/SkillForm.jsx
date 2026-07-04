0e0f8ce92265af7df42adf74bccfd106c84ea32e
roaayala <roaayala.ahm@gmail.com>
2026-07-04 18:05:52 +0800

    SkillModel

--- src/components/forms/SkillForm.jsx
+++ src/components/forms/SkillForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import FormHeader from "./FormHeader";
 import Button from "../commons/Button";
-import { Plus, ChevronDown, ChevronUp, Delete } from "lucide-react";
+import TextInput from "./TextInput";
+import { Plus, Save, Edit, Trash2, X } from "lucide-react";
 
 export default function SkillForm({ data, onSkillChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAdd, setIsAdd] = useState(false);
+  const [newSkill, setNewSkill] = useState("");
   const [editId, setEdit] = useState("");
 
   const handleOpen = (e) => {
@@ -12,38 +15,76 @@ export default function SkillForm({ data, onSkillChange }) {
     setIsOpen(!isOpen);
   };
 
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setIsAdd(true);
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    if (newSkill === "") return;
+
+    onSkillChange.add();
+
+    setNewSkill("");
+    setIsAdd(false);
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setIsAdd(false);
+  };
+
   return (
     <section>
       <FormHeader sectionTitle={"Skill"} isOpen={isOpen} onOpen={handleOpen} />
 
       {isOpen && (
-        <ul>
-          {data.length === 0 ? (
-            <li>No skill added!</li>
-          ) : (
-            data.map((skill) => (
-              <li key={skill.id} className="flex">
-                <Button
-                  onClick={(e) => e.preventDefault()}
-                  icon={<ChevronDown />}
-                />
+        <>
+          <ul>
+            {data.length === 0 ? (
+              <li>No skill added!</li>
+            ) : (
+              data.map((skill) => (
+                <li key={skill.id} className="flex">
+                  <span>{skill.name}</span>
 
-                <span>{skill.name}</span>
+                  <Button onClick={(e) => e.preventDefault()} icon={<Edit />} />
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      onSkillChange.delete(skill.id);
+                    }}
+                    icon={<Trash2 />}
+                  />
+                </li>
+              ))
+            )}
+          </ul>
 
-                <Button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    onSkillChange.delete(skill.id);
-                  }}
-                  icon={<Delete />}
-                />
-              </li>
-            ))
+          {isAdd && (
+            <TextInput
+              id={"skill"}
+              label={"New Skill"}
+              placeholder={"Skill"}
+              value={newSkill}
+              onChange={(e) => setNewSkill(e.target.value)}
+            />
           )}
-        </ul>
-      )}
 
-      {isOpen && <Button icon={<Plus />} text={"Add Skill"} />}
+          <div>
+            {isAdd ? (
+              <>
+                <Button icon={<X />} text={"Cancel"} onClick={handleCancel} />
+                <Button icon={<Save />} text={"Save"} onClick={handleSave} />
+              </>
+            ) : (
+              <Button icon={<Plus />} text={"Add Skill"} onClick={handleAdd} />
+            )}
+          </div>
+        </>
+      )}
     </section>
   );
 }
